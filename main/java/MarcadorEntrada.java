f3734d1d959be5823f6579b56296b67d4d6af8ad
Juan Pablo Vargas <38387499+PabloVargasG@users.noreply.github.com>
2020-02-16 18:22:47 -0600

    4to Commit de Prueba

---
+++ main/java/MarcadorEntrada.java
@@ -0,0 +1,7 @@
+import java.util.ArrayList;
+
+public class MarcadorEntrada {
+    public MarcadorEntrada(){
+        ArrayList<Persona> personas = new ArrayList<Persona>();
+    }
+}
---
+++ test/java/MarcadorJunitTest.java
@@ -0,0 +1,16 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class MarcadorJunitTest {
+    public MarcadorJunitTest() {
+    }
+
+    /**
+     * Test
+     */
+    @Test
+    public void testAdd() {
+
+    }
+}
